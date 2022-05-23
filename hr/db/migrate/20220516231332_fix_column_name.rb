66214197f924e412e531d5eb0222f9baaab7faf8
EC2 Default User <ec2-user@ip-172-31-15-185.ec2.internal>
2022-05-23 15:27:56 +0000

    moved education <hr>

--- hr/db/migrate/20220516231332_fix_column_name.rb
+++ hr/db/migrate/20220516231332_fix_column_name.rb
@@ -1,7 +1,6 @@
 class FixColumnName < ActiveRecord::Migration[5.0]
   def self.up
     rename_column :work_experiences, :duration, :startdate
-    rename_column :educations, :name, :school
   end
 
   def self.down
